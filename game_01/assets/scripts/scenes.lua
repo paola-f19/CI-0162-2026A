82c7a9ad8e6b5c1e7e99e5b19470f334bb21f26c
Paola Feng <paola.feng@ucr.ac.cr>
2026-05-10 00:55:07 -0600

    Added level 3 map

--- game_01/assets/scripts/scenes.lua
+++ game_01/assets/scripts/scenes.lua
@@ -1,6 +1,7 @@
 
 scenes = {
   [0] = 
+  {name = "level_03", path = "./assets/scripts/scene_03.lua"},
   {name = "main_menu", path = "./assets/scripts/scene_menu.lua"},
   {name = "level_02", path = "./assets/scripts/scene_02.lua"},
   {name = "level_01", path = "./assets/scripts/scene_01.lua"},
